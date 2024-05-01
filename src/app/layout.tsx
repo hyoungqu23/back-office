5ac5ad75a03e5def0c0c5355f3eecbb65c9abb39
HyoungMin <hyoungqu23@gmail.com>
2024-05-02 01:58:29 +0900

    💄 [#1] Next.js의 LocalFont를 활용해 Pretendard를 추가한다

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
+import localFont from 'next/font/local';
 
 import '../layers/apps/styles/globals.css';
 
@@ -16,9 +15,39 @@ const RootLayout = ({
 }>) => {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={pretendard.className}>{children}</body>
     </html>
   );
 };
 
 export default RootLayout;
+
+const pretendard = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Pretendard-ExtraBold.subset.woff2',
+      weight: '800',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Pretendard-Bold.subset.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Pretendard-SemiBold.subset.woff2',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Pretendard-Medium.subset.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Pretendard-Regular.subset.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+  ],
+});
