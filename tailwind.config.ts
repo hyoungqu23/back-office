9e5ec7d267c18f8dbaf581c8b65ce5e52305a154
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2024-05-19 20:30:28 +0900

    🔧 [#1] Tailwind Config의 Content를 FSD 디렉토리 구조에 맞게 수정한다

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,8 +2,12 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    './src/layers/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/root/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/widgets/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/features/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/entities/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/shared/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {},
