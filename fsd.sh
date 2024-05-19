bc7ceb52edd8e42103b9cd173daa38b7caa4ff7c
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2024-05-19 20:33:37 +0900

    🔨 [#1] FSD 아키텍처 디렉토리 생성 스크립트를 업데이트한다

--- fsd.sh
+++ fsd.sh
@@ -3,20 +3,26 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-base_dir="src/layers"
+base_dir="src"
 target_dir="$base_dir/$1"
 mkdir -p $target_dir
 
 last_part=$(basename $1)
-formatted_name=$(echo $last_part | sed -e "s/\(.*\)/\u\1/")
+formatted_name=$(echo $last_part | sed -e "s/\(.*\)/\1/")
 
-echo "// $formatted_name Public API" > "$target_dir/index.ts"
+echo "// export * from './api';" >> "$target_dir/index.ts"
+echo "// export * from './config';" >> "$target_dir/index.ts"
+echo "// export * from './constant';" >> "$target_dir/index.ts"
+echo "// export * from './lib';" >> "$target_dir/index.ts"
+echo "// export * from './model';" >> "$target_dir/index.ts"
+echo "// export * from './type';" >> "$target_dir/index.ts"
+echo "// export * from './ui';" >> "$target_dir/index.ts"
 
 declare -a subdirs=("api" "model" "type" "config" "constant" "ui" "lib")
 
 for subdir in "${subdirs[@]}"; do
   mkdir -p "$target_dir/$subdir"
-  echo "// $subdir of $formatted_name Public API" > "$target_dir/$subdir/index.ts"
+  echo "" > "$target_dir/$subdir/index.ts"
 done
 
 echo "$target_dir 생성 완료"
